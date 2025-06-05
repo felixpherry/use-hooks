d711da4938b784a3168090f6daaaa74ec8d0ee52
Felix Pherry <182051.FELIX@klgroup.local>
2025-06-05 10:24:40 +0700

    feat: implement useDebounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,18 @@
-import useObjectState from './hooks/016-useObjectState/useObjectState';
-
-const initialState = {
-  team: 'Utah Jazz',
-  wins: 2138,
-  losses: 1789,
-  championships: 0,
-};
+import React from 'react';
+import useDebounce from './hooks/017-useDebounce/useDebounce';
 
 export default function App() {
-  const [stats, setStats] = useObjectState(initialState);
-
-  const addWin = () => {
-    setStats((s) => ({
-      wins: s.wins + 1,
-    }));
-  };
-
-  const addLoss = () => {
-    setStats((s) => ({
-      losses: s.losses + 1,
-    }));
-  };
-
-  const addChampionship = () => {
-    setStats((s) => ({
-      championships: s.championships + 1,
-    }));
-  };
-
-  const reset = () => {
-    setStats(initialState);
-  };
-
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
   return (
-    <section>
-      <h1>useObjectState</h1>
-
-      <button className='link' onClick={addWin}>
-        Add Win
-      </button>
-      <button className='link' onClick={addLoss}>
-        Add Loss
-      </button>
-
-      <button className='link' onClick={addChampionship}>
-        Add Championship
-      </button>
-      <button className='link' onClick={reset}>
-        Reset
-      </button>
-
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(stats).map((key) => {
-              return <th>{key}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {Object.keys(stats).map((key) => {
-              return <td>{`${stats[key as keyof typeof stats]}`}</td>;
-            })}
-          </tr>
-        </tbody>
-      </table>
-    </section>
+    <div>
+      <input
+        type='search'
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+      />
+      <p>{searchTerm}</p>
+      <p>{debouncedSearchTerm}</p>
+    </div>
   );
 }
